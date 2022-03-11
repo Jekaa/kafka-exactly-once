5ff46bb95d9d35ec14f442632ca8e7096704ab93
GordienkoEE <gordienko.e.evg@sberbank.ru>
2022-03-11 11:26:47 +0300

    init commit

---
+++ src/main/java/com/learning/kafkaexactlyonce/KafkaExactlyOnceApplication.java
@@ -0,0 +1,13 @@
+package com.learning.kafkaexactlyonce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExactlyOnceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExactlyOnceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/kafkaexactlyonce/KafkaExactlyOnceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.kafkaexactlyonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaExactlyOnceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
